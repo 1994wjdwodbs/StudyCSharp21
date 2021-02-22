cd405563fef3aa7b26d9b5c3fc80d502f7683c3d
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-02-22 17:06:05 +0900

    C# Practice

---
+++ chap03/Chap03App/ConstTestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstTestApp
+{
+    class Program
+    {
+        enum Season
+        {
+            Spring, Summer, Fall, Winter
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("상수 테스트");
+            const double PI = 3.141592; // 상수
+            Console.WriteLine($"원주율 값 : {PI}");
+
+            Season mySeason = Season.Summer;
+            mySeason = Season.Winter;
+            Console.WriteLine($"Weather : {mySeason}");
+            int a = 0;
+            int? b = null;
+            Console.WriteLine($"a : {a}");
+            Console.WriteLine($"b : {b}");
+        }
+    }
+}
---
+++ chap03/Chap03App/IntConversionApp/ConvTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntConversionApp
+{
+    class ConvTest
+    {
+        static void Main(string[] args)
+        {
+            short shMaxVal = short.MaxValue;
+            int inCastVal = 0;
+
+            inCastVal = shMaxVal;
+            Console.WriteLine($"short값 변환한 int값은 {inCastVal}");
+
+            inCastVal = +5;
+            short shCastVal = (short) inCastVal;
+            Console.WriteLine($"int값 변환한 short값은 {shCastVal}"); // Overflow
+
+            float flVal = 3.141592f;
+            inCastVal = (int)flVal;
+            Console.WriteLine($"float값 변환한 int값은 {inCastVal}");
+            double dlVal = inCastVal;
+            Console.WriteLine($"int값 변환한 double값은 {dlVal}");
+
+            object obj = 20; // Boxing
+            int inUnboxingVal = (int) obj; // UnBoxing
+
+            string strVal = "200";
+            int result = Convert.ToInt32(strVal) * 3;
+        }
+    }
+}
