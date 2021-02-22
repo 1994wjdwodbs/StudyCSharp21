5583f5b4b99d7ed28bbf9dc867c349493bdc4bed
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-02-22 17:27:31 +0900

    4장 커밋

---
+++ chap04/Chap04App/Chap04App/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap04App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("자연수 입력 : ");
+            int values;
+            values = int.Parse(Console.ReadLine());
+
+            if (values % 3 == 0)
+                Console.WriteLine("3의 배수");
+            else
+                Console.WriteLine("3의 배수가 아님");
+
+            if (values % 7 == 0)
+                Console.WriteLine("7의 배수");
+            else
+                Console.WriteLine("7의 배수가 아님");
+        }
+    }
+}
