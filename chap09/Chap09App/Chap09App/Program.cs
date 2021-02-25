25c59372b035b7ee8a1986178d26ea441c745ff2
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-02-25 16:48:26 +0900

    0225 C# 학습 커밋

---
+++ chap09/Chap09App/Chap09App/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap09App
+{
+    class MyClass
+    {
+        private int iValue; 
+        // 0 ~ 120 값만 입력받는 멤버변수
+        private double dZeta;
+        private float fPng;
+        private string strVal;
+        private int inCode;
+
+        // 프로퍼티
+        public int IValue
+        {
+            get 
+            {
+                return this.iValue;
+            }
+            set 
+            {
+                // 여기서 말하는 value는 setXXX(int value)를 말함
+                if (value < 0)
+                    this.iValue = 0;
+                else if (value > 120)
+                    this.iValue = 120;
+                else
+                    this.iValue = value;
+            }
+        }
+
+        public MyClass(int iValue)
+        {
+            IValue = iValue;
+        }
+
+        public void PrintValue()
+        {
+            Console.WriteLine($"값은 {this.iValue}");
+        }
+    }
+
+    class Base_ 
+    { 
+        public void Out()
+        {
+            Console.WriteLine("B()");
+        }
+    }
+
+    class Derived_ : Base_
+    {
+        public new void Out()
+        {
+            Console.WriteLine("D()");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass(1200);
+            myClass.PrintValue();
+
+            myClass.IValue = 108;
+            myClass.PrintValue();
+            Console.WriteLine($"현재 온도는 {myClass.IValue}°C 입니다.");
+
+            Base_ B = new Derived_();
+            B.Out();
+
+            // Derived_ D = (Derived_) new Base_();
+            // D.Out();
+
+        }
+    }
+}
---
+++ chap09/Chap09App/PropertyTestApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTestApp
+{
+    // 생일정보 클래스
+    class BirthdayInfo
+    {
+        private string name;
+        private DateTime birthday;
+
+        // 프로퍼티
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        // 프로퍼티
+        public DateTime BBirthday
+        {
+            get { return this.birthday; }
+            set { this.birthday = value; }
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public DateTime GetBirthDay()
+        {
+            return this.birthday;
+        }
+        public void SetBirthDay(DateTime birthday)
+        {
+            this.birthday = birthday;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo info = new BirthdayInfo();
+            info.SetName("홍길동");
+            info.SetBirthDay(new DateTime(1990, 1, 8));
+
+            Console.WriteLine($"이름 : {info.GetName()}");
+            Console.WriteLine($"생일 : {info.GetBirthDay()}");
+
+            BirthdayInfo info2 = new BirthdayInfo();
+            info2.Name = "홍길순";
+            Console.WriteLine($"이름 : {info2.Name}");
+            info2.BBirthday = new DateTime(1992, 3, 16);
+            Console.WriteLine($"생일 : {info2.BBirthday}");
+        }
+    }
+}
