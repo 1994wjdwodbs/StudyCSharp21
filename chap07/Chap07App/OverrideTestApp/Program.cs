c207a9294433252101a5f9fc34d9af4f7d1c697e
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-02-25 13:10:07 +0900

    0225 오전 수업 내용 커밋

---
+++ chap07/Chap07App/OverrideTestApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideTestApp
+{
+    class ArmorSuite
+    {
+        // 자식클래스에서 재정의할 것
+        public virtual void Initialize()
+        {
+            Console.WriteLine("ArmorSuite 초기화!");
+        }
+    }
+
+    class IronMan : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("레이저 펄스 장착!");
+        }
+    }
+
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("ArmorSuite개량 Warmachine 초기화!");
+            Console.WriteLine("Double-Barrel 캐논 장착!");
+            Console.WriteLine("Micro-Rocket 런처 장착!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ArmorSuite 생산");
+            ArmorSuite suite = new ArmorSuite();
+            suite.Initialize();
+
+            Console.WriteLine("WarMachine 생산");
+            // WarMachine machine = new ArmorSuite() as WarMachine;
+            WarMachine machine = new WarMachine();
+            machine.Initialize();
+
+            Console.WriteLine("IronMan 생산!");
+            IronMan ironMan = new IronMan();
+            ironMan.Initialize();
+        }
+    }
+}
