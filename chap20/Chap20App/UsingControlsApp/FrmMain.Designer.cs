5cbbe33e79965744c39e7e1c304e4083aa776c26
JYJ_FROM_KOOKBI <1994wjdwodbs@gmail.com>
2021-03-08 11:30:31 +0900

    C# 20장 컨트롤 커밋

--- chap20/Chap20App/UsingControlsApp/FrmMain.Designer.cs
+++ chap20/Chap20App/UsingControlsApp/FrmMain.Designer.cs
@@ -31,17 +31,29 @@ namespace UsingControlsApp
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.CboFont = new System.Windows.Forms.ComboBox();
-            this.ChkItalic = new System.Windows.Forms.CheckBox();
-            this.ChkBold = new System.Windows.Forms.CheckBox();
             this.TxtResult = new System.Windows.Forms.TextBox();
+            this.ChkBold = new System.Windows.Forms.CheckBox();
+            this.ChkItalic = new System.Windows.Forms.CheckBox();
+            this.CboFont = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.TrbHandle = new System.Windows.Forms.TrackBar();
             this.PrbDisplay = new System.Windows.Forms.ProgressBar();
+            this.TrbHandle = new System.Windows.Forms.TrackBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.BtnModal = new System.Windows.Forms.Button();
+            this.BtnModaless = new System.Windows.Forms.Button();
+            this.BtnMsgBox = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.TrvDummy = new System.Windows.Forms.TreeView();
+            this.LsvDummy = new System.Windows.Forms.ListView();
+            this.BtnAddRoot = new System.Windows.Forms.Button();
+            this.BtnAddChild = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.TrbHandle)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -58,35 +70,12 @@ namespace UsingControlsApp
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "ComboBox, CheckBox, TextBox";
             // 
-            // label1
+            // TxtResult
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Font : ";
-            // 
-            // CboFont
-            // 
-            this.CboFont.FormattingEnabled = true;
-            this.CboFont.Location = new System.Drawing.Point(65, 20);
-            this.CboFont.Name = "CboFont";
-            this.CboFont.Size = new System.Drawing.Size(214, 20);
-            this.CboFont.TabIndex = 1;
-            this.CboFont.SelectedIndexChanged += new System.EventHandler(this.CboFont_SelectedIndexChanged);
-            // 
-            // ChkItalic
-            // 
-            this.ChkItalic.AutoSize = true;
-            this.ChkItalic.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.ChkItalic.Location = new System.Drawing.Point(402, 19);
-            this.ChkItalic.Name = "ChkItalic";
-            this.ChkItalic.Size = new System.Drawing.Size(60, 16);
-            this.ChkItalic.TabIndex = 2;
-            this.ChkItalic.Text = "이탤릭";
-            this.ChkItalic.UseVisualStyleBackColor = true;
-            this.ChkItalic.CheckedChanged += new System.EventHandler(this.ChkItalic_CheckedChanged);
+            this.TxtResult.Location = new System.Drawing.Point(20, 58);
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(442, 21);
+            this.TxtResult.TabIndex = 4;
             // 
             // ChkBold
             // 
@@ -100,12 +89,35 @@ namespace UsingControlsApp
             this.ChkBold.UseVisualStyleBackColor = true;
             this.ChkBold.CheckedChanged += new System.EventHandler(this.ChkBold_CheckedChanged);
             // 
-            // TxtResult
+            // ChkItalic
             // 
-            this.TxtResult.Location = new System.Drawing.Point(20, 58);
-            this.TxtResult.Name = "TxtResult";
-            this.TxtResult.Size = new System.Drawing.Size(442, 21);
-            this.TxtResult.TabIndex = 4;
+            this.ChkItalic.AutoSize = true;
+            this.ChkItalic.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ChkItalic.Location = new System.Drawing.Point(402, 19);
+            this.ChkItalic.Name = "ChkItalic";
+            this.ChkItalic.Size = new System.Drawing.Size(60, 16);
+            this.ChkItalic.TabIndex = 2;
+            this.ChkItalic.Text = "이탤릭";
+            this.ChkItalic.UseVisualStyleBackColor = true;
+            this.ChkItalic.CheckedChanged += new System.EventHandler(this.ChkItalic_CheckedChanged);
+            // 
+            // CboFont
+            // 
+            this.CboFont.FormattingEnabled = true;
+            this.CboFont.Location = new System.Drawing.Point(65, 20);
+            this.CboFont.Name = "CboFont";
+            this.CboFont.Size = new System.Drawing.Size(214, 20);
+            this.CboFont.TabIndex = 1;
+            this.CboFont.SelectedIndexChanged += new System.EventHandler(this.CboFont_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Font : ";
             // 
             // groupBox2
             // 
@@ -118,6 +130,14 @@ namespace UsingControlsApp
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "TrackBar, ProgressBar";
             // 
+            // PrbDisplay
+            // 
+            this.PrbDisplay.Location = new System.Drawing.Point(20, 58);
+            this.PrbDisplay.Maximum = 20;
+            this.PrbDisplay.Name = "PrbDisplay";
+            this.PrbDisplay.Size = new System.Drawing.Size(442, 23);
+            this.PrbDisplay.TabIndex = 1;
+            // 
             // TrbHandle
             // 
             this.TrbHandle.Location = new System.Drawing.Point(20, 20);
@@ -127,23 +147,112 @@ namespace UsingControlsApp
             this.TrbHandle.TabIndex = 0;
             this.TrbHandle.Scroll += new System.EventHandler(this.TrbHandle_Scroll);
             // 
-            // PrbDisplay
+            // groupBox3
             // 
-            this.PrbDisplay.Location = new System.Drawing.Point(20, 58);
-            this.PrbDisplay.Maximum = 20;
-            this.PrbDisplay.Name = "PrbDisplay";
-            this.PrbDisplay.Size = new System.Drawing.Size(442, 23);
-            this.PrbDisplay.TabIndex = 1;
+            this.groupBox3.Controls.Add(this.BtnMsgBox);
+            this.groupBox3.Controls.Add(this.BtnModaless);
+            this.groupBox3.Controls.Add(this.BtnModal);
+            this.groupBox3.Location = new System.Drawing.Point(12, 261);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(480, 110);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Modal, Modaless";
+            // 
+            // BtnModal
+            // 
+            this.BtnModal.Location = new System.Drawing.Point(101, 20);
+            this.BtnModal.Name = "BtnModal";
+            this.BtnModal.Size = new System.Drawing.Size(75, 35);
+            this.BtnModal.TabIndex = 0;
+            this.BtnModal.Text = "Modal";
+            this.BtnModal.UseVisualStyleBackColor = true;
+            this.BtnModal.Click += new System.EventHandler(this.BtnModal_Click);
+            // 
+            // BtnModaless
+            // 
+            this.BtnModaless.Location = new System.Drawing.Point(198, 20);
+            this.BtnModaless.Name = "BtnModaless";
+            this.BtnModaless.Size = new System.Drawing.Size(75, 35);
+            this.BtnModaless.TabIndex = 1;
+            this.BtnModaless.Text = "Modaless";
+            this.BtnModaless.UseVisualStyleBackColor = true;
+            this.BtnModaless.Click += new System.EventHandler(this.BtnModaless_Click);
+            // 
+            // BtnMsgBox
+            // 
+            this.BtnMsgBox.Location = new System.Drawing.Point(295, 20);
+            this.BtnMsgBox.Name = "BtnMsgBox";
+            this.BtnMsgBox.Size = new System.Drawing.Size(95, 35);
+            this.BtnMsgBox.TabIndex = 2;
+            this.BtnMsgBox.Text = "MessageBox";
+            this.BtnMsgBox.UseVisualStyleBackColor = true;
+            this.BtnMsgBox.Click += new System.EventHandler(this.BtnMsgBox_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.BtnAddChild);
+            this.groupBox4.Controls.Add(this.BtnAddRoot);
+            this.groupBox4.Controls.Add(this.LsvDummy);
+            this.groupBox4.Controls.Add(this.TrvDummy);
+            this.groupBox4.Location = new System.Drawing.Point(12, 377);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(480, 264);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "TreeView && ListView";
+            // 
+            // TrvDummy
+            // 
+            this.TrvDummy.Location = new System.Drawing.Point(40, 20);
+            this.TrvDummy.Name = "TrvDummy";
+            this.TrvDummy.Size = new System.Drawing.Size(189, 160);
+            this.TrvDummy.TabIndex = 4;
+            // 
+            // LsvDummy
+            // 
+            this.LsvDummy.HideSelection = false;
+            this.LsvDummy.Location = new System.Drawing.Point(238, 20);
+            this.LsvDummy.Name = "LsvDummy";
+            this.LsvDummy.Size = new System.Drawing.Size(205, 160);
+            this.LsvDummy.TabIndex = 5;
+            this.LsvDummy.UseCompatibleStateImageBehavior = false;
+            this.LsvDummy.View = System.Windows.Forms.View.Details;
+            // 
+            // BtnAddRoot
+            // 
+            this.BtnAddRoot.Location = new System.Drawing.Point(40, 186);
+            this.BtnAddRoot.Name = "BtnAddRoot";
+            this.BtnAddRoot.Size = new System.Drawing.Size(75, 36);
+            this.BtnAddRoot.TabIndex = 6;
+            this.BtnAddRoot.Text = "루트추가";
+            this.BtnAddRoot.UseVisualStyleBackColor = true;
+            this.BtnAddRoot.Click += new System.EventHandler(this.BtnAddRoot_Click);
+            // 
+            // BtnAddChild
+            // 
+            this.BtnAddChild.Location = new System.Drawing.Point(121, 186);
+            this.BtnAddChild.Name = "BtnAddChild";
+            this.BtnAddChild.Size = new System.Drawing.Size(75, 36);
+            this.BtnAddChild.TabIndex = 7;
+            this.BtnAddChild.Text = "버튼추가";
+            this.BtnAddChild.UseVisualStyleBackColor = true;
+            this.BtnAddChild.Click += new System.EventHandler(this.BtnAddChild_Click);
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 365);
+            this.ClientSize = new System.Drawing.Size(504, 653);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Using Control App";
             this.Load += new System.EventHandler(this.FrmMain_Load);
             this.groupBox1.ResumeLayout(false);
@@ -151,6 +260,8 @@ namespace UsingControlsApp
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.TrbHandle)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -166,6 +277,16 @@ namespace UsingControlsApp
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.ProgressBar PrbDisplay;
         private System.Windows.Forms.TrackBar TrbHandle;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button BtnModal;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button BtnMsgBox;
+        private System.Windows.Forms.Button BtnModaless;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button BtnAddChild;
+        private System.Windows.Forms.Button BtnAddRoot;
+        private System.Windows.Forms.ListView LsvDummy;
+        private System.Windows.Forms.TreeView TrvDummy;
     }
 }
 
--- chap20/Chap20App/UsingControlsApp/FrmMain.cs
+++ chap20/Chap20App/UsingControlsApp/FrmMain.cs
@@ -13,32 +13,25 @@ namespace UsingControlsApp
     public partial class FrmMain : Form
     {
         FontStyle style = FontStyle.Regular;
+        Random random = new Random(37);
 
         public FrmMain()
         {
             InitializeComponent();
         }
 
-        private void ChangeFont()
-        {
-            if(CboFont.SelectedIndex >= 0 && CboFont.SelectedIndex < FontFamily.Families.Length)
-            {
-                TxtResult.Font = new Font((string)CboFont.SelectedItem, 14, style);
-                // TxtResult.Text = "Sample";
-            }
-
-            return;
-        }
-
+        #region 이벤트 핸들러 영역
         private void FrmMain_Load(object sender, EventArgs e)
         {
+            LsvDummy.Columns.Add("Name");
+            LsvDummy.Columns.Add("Depth");
+
             var FontsList = FontFamily.Families;
-            foreach(var font in FontsList)
+            foreach (var font in FontsList)
             {
                 CboFont.Items.Add(font.Name);
             }
         }
-
         private void ChkBold_CheckedChanged(object sender, EventArgs e)
         {
             //     일반 텍스트입니다.
@@ -98,5 +91,95 @@ namespace UsingControlsApp
         {
             PrbDisplay.Value = TrbHandle.Value;
         }
+
+        /* * * * * * * * * * * * * *
+         * 모달 윈도(Modal Window)는 사용자 인터페이스 디자인 개념에서 자식 윈도에서 부모 윈도로 돌아가기 전에 
+         * 사용자의 상호동작을 요구하는 창을 말한다. 그래서 응용 프로그램의 주 창의 작업 흐름을 방해한다. 
+         * 일반적으로 모달 대화상자로 불리는데, 그 이유는 대화상자를 부를 때 흔히 사용되기 때문이다. 
+         * 우리가 자주 사용하는 파일 열기/저장 대화상자를 생각하면 이해하기 쉽다.
+         * 
+         * 모달 창들은 일반적으로 사용자의 명령을 인식하기 위해서나, 긴급 상황을 알리기 위해 많이 사용된다. 
+         * 웹에서는 이미지 작업을 예로 든다면 이미지를 자세하게 볼 때 자주 사용된다.
+         * * * * * * * * * * * * * */
+        private void BtnModal_Click(object sender, EventArgs e)
+        {
+            Form frm = new Form();
+            frm.Text = "Modal Form";
+            frm.Width = 300;
+            frm.Height = 100;
+            frm.BackColor = Color.Coral;
+            frm.ShowDialog(); // 모달창 띄우기
+            bool flag = false;
+            flag ^= true;
+        }
+
+        private void BtnModaless_Click(object sender, EventArgs e)
+        {
+            Form frm = new Form // Anonymous Class (무명 클래스)
+            {
+                Text = "Modaless Form",
+                Width = 300,
+                Height = 100,
+                BackColor = Color.GreenYellow
+            };
+            frm.Show(); // 모달리스창 띄우기
+
+        }
+
+        private void BtnMsgBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(TxtResult.Text, "타이틀", MessageBoxButtons.YesNo, MessageBoxIcon.Warning); // 모달창 메시지박스
+        }
+
+        private void BtnAddRoot_Click(object sender, EventArgs e)
+        {
+            TrvDummy.Nodes.Add(random.Next().ToString());
+
+            DisplayTreeToList();
+        }
+
+        private void BtnAddChild_Click(object sender, EventArgs e)
+        {
+            if (TrvDummy.SelectedNode == null)
+            {
+                MessageBox.Show("선택된 노드가 없습니다", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            TrvDummy.SelectedNode.Nodes.Add(random.Next().ToString());
+            TrvDummy.SelectedNode.Expand();
+
+            DisplayTreeToList();
+        }
+        #endregion
+
+        #region 사용자 메서드 영역
+        private void ChangeFont()
+        {
+            if (CboFont.SelectedIndex >= 0 && CboFont.SelectedIndex < FontFamily.Families.Length)
+            {
+                TxtResult.Font = new Font((string)CboFont.SelectedItem, 14, style);
+                // TxtResult.Text = "Sample";
+            }
+
+            return;
+        }
+
+        public void DisplayTreeToList()
+        {
+            LsvDummy.Items.Clear();
+            foreach (TreeNode node in TrvDummy.Nodes)
+                DisplayTreeToList(node);
+        }
+
+        private void DisplayTreeToList(TreeNode node)
+        {
+            // throw new NotImplementedException();
+            LsvDummy.Items.Add(new ListViewItem(new string[] {node.Text, node.FullPath}));
+
+            foreach (TreeNode item in node.Nodes)
+                DisplayTreeToList(item); // 재귀
+        }
+        #endregion
+
     }
 }
